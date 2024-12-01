5e508cf6b96e9bcdc145f160d3ea1e3a9dc9e01c
Sivabalan Prabhu <sivabalanprabhu44@gmail.com>
2024-12-01 19:36:55 +0530

    variables, conditions, loop and functions added

--- main.go
+++ main.go
@@ -2,4 +2,30 @@ package main
 import "fmt"
 func main() {
     fmt.Println("Hello, Go!")
+
+	var name string = "John Doe"
+	var age int = 25
+	
+	fmt.Printf("Name: %s\nAge: %d\n", name,age)
+
+	batsman := "David Warner"
+	runs := 34
+	fmt.Printf("Batsmen: %s\nRuns: %d\n", batsman, runs)
+
+	if runs >= 50 {
+		fmt.Println("Half-century!")
+	} else{
+		fmt.Println("Not a half-century!")
+	}
+
+	for i := 0; i < 5; i++ {
+		fmt.Println(i)
+	}
+	
+	fmt.Println(add(5,6))
+	
+}
+
+func add(a int, b int) int{
+	return a + b
 }
